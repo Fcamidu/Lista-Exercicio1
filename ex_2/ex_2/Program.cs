1cde38dfccdbf046d144fda8cef95b87b9049141
Mateus <fcamidumateus345@gmail.com>
2025-04-16 22:25:39 -0300

    Add files via upload

---
+++ ex_2/ex_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int aresta;
+            int area;
+
+            Console.Write("Informe o valor da aresta:");
+            aresta = int.Parse(Console.ReadLine());
+
+            area = aresta * aresta;
+
+            Console.WriteLine("O valor da area é:" + area);   
+
+        }
+    }
+}
