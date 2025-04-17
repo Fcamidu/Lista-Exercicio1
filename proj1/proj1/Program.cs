94a7183b3eb23c4928471671b515470414851887
Mateus <fcamidumateus345@gmail.com>
2025-04-16 21:58:55 -0300

    Add files via upload

---
+++ proj1/proj1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proj1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bas3;
+            int altura;
+            int area;
+
+            Console.Write("informe o valor da base do retangulo:");
+            bas3 = int.Parse(Console.ReadLine());
+
+            Console.Write("informe o valor da altura do retangulo:");
+            altura = int.Parse(Console.ReadLine());
+
+            area = bas3 * altura;
+
+            Console.WriteLine("a area de seu retangulo é:" + area);
+        }
+    }
+}
